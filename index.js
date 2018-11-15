d024ae41c3324c73d7a36e7e59b0b3557ad03ff7
sg3957 <sg3957@nyu.edu>
2018-11-14 21:31:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+
+//function 1
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue > 2000  && someValue < 2500) {
+  return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+}
+
+//function 2
+function ternaryCheckCity(someCity){
+
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+//function 3
+function switchOnCharmFromTip(charm){
+  switch (charm) {
+    case 'generous':
+        return 'Thank you so much.'
+    case 'not as generous':
+        return 'Thank you.'
+    case 'thanks for everything':
+        return 'Bye.'
+    }
+}

761b39b49ae31ebae71af10d8a08402d8c1e8e10
Veraprot <vera.protopopova@gmail.com>
2018-11-17 13:38:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet > 2500) {
+    result = "No can do."
+  } else if (feet > 2000) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet <= 400) {
+    result = "This one is on me!";
+  }
+  return result;
+}
+ function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+ }
+ function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      result = 'Thank you.'
+      break;
+    default:
+      result = 'Bye.'
+      break;
+  }
+  return result;
+} 

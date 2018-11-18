6e8ff4fcc18dea59dcdde7f5deac2fc98a6719b2
minheekangg <minheekangg@gmail.com>
2018-11-17 21:29:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride > 2500){
+    result = "No can do."
+  }
+  else if (ride >= 2000) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (ride <= 400) {
+    result = "This one is on me!";
+  }
+   return result
+};
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous' :
+      return "Thank you so much.";
+      break;
+    case 'not as generous' :
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+    }
+}
